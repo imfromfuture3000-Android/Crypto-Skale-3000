6738bf986369fcdfd352482ebd2a4e8cc112a17f
Imfromfuture3000 <imfromfuture3000@gmail.com>
2025-08-22 21:57:14 +0800

    deploy.js

---
+++ scripts/deploy.js
@@ -0,0 +1,11 @@
+async function main() {
+  const Vault = await ethers.getContractFactory("Vault");
+  const vault = await Vault.deploy();
+  await vault.deployed();
+  console.log("✅ Vault deployed to:", vault.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
